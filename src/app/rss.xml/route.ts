eac73c4a9b2dd0fb1b300224358381c6ae98e985
bsn652 <2697744811@qq.com>
2026-06-24 15:14:19 +0800

    Fix RSS route for static export

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,41 +1,5 @@
-import { siteConfig } from '@/lib/site-config';
-import { getAllPosts } from '@/lib/posts';
-
+// RSS route disabled for static export
+// To re-enable: remove the "output: export" from next.config.ts
 export async function GET() {
-  const posts = getAllPosts();
-  const siteUrl = siteConfig.url;
-
-  const items = posts
-    .map(
-      (post) => `
-    <item>
-      <title><![CDATA[${post.title}]]></title>
-      <description><![CDATA[${post.description}]]></description>
-      <link>${siteUrl}/articles/${post.slug}</link>
-      <guid>${siteUrl}/articles/${post.slug}</guid>
-      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-      ${post.tags.map((tag) => `<category>${tag}</category>`).join('\n      ')}
-    </item>`
-    )
-    .join('');
-
-  const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>${siteConfig.name}</title>
-    <description>${siteConfig.description}</description>
-    <link>${siteUrl}</link>
-    <atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml"/>
-    <language>zh-CN</language>
-    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    ${items}
-  </channel>
-</rss>`;
-
-  return new Response(rss, {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'public, max-age=3600',
-    },
-  });
+  return new Response(null, { status: 404 });
 }
